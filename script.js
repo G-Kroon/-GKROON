199147eaef802970e3dfdb0852c2d0e61025fe41
Jonathan Peters <72918619+QMS85@users.noreply.github.com>
2025-04-15 12:52:46 +0200

    Update script.js
    Signed-off-by: Jonathan Peters <72918619+QMS85@users.noreply.github.com>

--- script.js
+++ script.js
@@ -429,3 +429,27 @@ function submitForm(event) {
     submissions.push(formData);
     localStorage.setItem('paymentSubmissions', JSON.stringify(submissions));
 }
+
+      // EMAIL JS JAVASCRIPT CODE
+              (function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init({
+              publicKey: "YOUR_PUBLIC_KEY",
+            });
+        })();
+    </script>
+    <script type="text/javascript">
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
+// END OF EMAIL JS JAVASCRIPT CODE
+
